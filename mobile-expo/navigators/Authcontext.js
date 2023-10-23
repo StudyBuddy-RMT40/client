d314f949b5b4aeb5d0979a00c88e6c829e5d0cd5
Najmi Aziz <najmiabo@gmail.com>
2023-10-23 20:02:59 +0700

    docs: change use state to false

--- mobile-expo/navigators/Authcontext.js
+++ mobile-expo/navigators/Authcontext.js
@@ -3,7 +3,7 @@ import React, { createContext, useState, useContext } from "react";
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-  const [isLoggedIn, setIsLoggedIn] = useState(true);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [users, setUsers] = useState([]);
   const [currentUser, setCurrentUser] = useState(null);
 
