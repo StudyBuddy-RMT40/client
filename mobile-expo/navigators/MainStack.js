4e9db9c9b0d7e88da6d292a53aa1735dd191f939
andraandrika12 <andraandrika12@gmail.com>
2023-10-23 14:24:51 +0700

    feat: ProjectScreen and VerticalScreen

--- mobile-expo/navigators/MainStack.js
+++ mobile-expo/navigators/MainStack.js
@@ -1,5 +1,7 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import LandingPageScreen from "../screen/LandingPageScreen";
+import DetailScreen from "../screen/DetailScreen";
+import ProjectScreen from "../screen/ProjectScreen";
 
 const Stack = createNativeStackNavigator();
 
@@ -9,6 +11,16 @@ export default function LandingPageStack() {
       <Stack.Screen
         name="LandingPage"
         component={LandingPageScreen}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="Detail"
+        component={DetailScreen}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="Project"
+        component={ProjectScreen}
         options={{ headerShown: false }}
       />
     </Stack.Navigator>
