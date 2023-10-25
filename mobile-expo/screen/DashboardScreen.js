c1c552016946b23ec9a1b30c8ed7c5f9b85dedee
Riska <riskastw@gmail.com>
2023-10-25 18:22:09 +0700

    fix: handling form and missing screens

--- mobile-expo/screen/DashboardScreen.js
+++ mobile-expo/screen/DashboardScreen.js
@@ -88,6 +88,9 @@ export default function DashboardScreen() {
     },
   ]);
   let temp = [];
+  // const finishedProjects = projectDataFromRedux
+  // ? projectDataFromRedux.filter((project) => project.status === "Finished")
+  // : [];
 
   const buttonItems = [
     {
